79b1eb84144871b4fa215b9b12326071d255b62b
murugan <murugananthamv2005@gmail.com>
2026-09-08 22:08:08 +0530

    variable and int data type and its memory spaces

---
+++ fmentals.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <limits.h>
+int main(void){
+    // int var=4;
+    // int var1=1;
+    // var=5;
+    // printf("%d",var);
+    // printf("hello embedded World\n");
+    // return 0;
+// -----------------------------------------#size of int
+    // int one=INT_MIN;
+    // int two=INT_MAX;
+    // short int three=SHRT_MIN;
+    // short int four=SHRT_MAX;
+    // long int few=INT_MIN;
+    // long int six=INT_MAX;
+    // unsigned short int seven=0;
+    // unsigned short int eight=USHRT_MAX;
+    // unsigned long int nine=0;
+    // unsigned long int ten=ULONG_MAX;
+    // printf("size of signed int:%d to %d",one,two);
+    // printf("size of short int:%d to %d",three,four);
+    // printf("size of long int:%d to %d",few,six);
+    // printf("size of unsigned short int:%lu to %lu",seven,eight);
+    // printf("size of unsigned long int:%lu to %lu",nine,ten);
+
+//
+//-------------------------------
+//
+//
+
+
+}

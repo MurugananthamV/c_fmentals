40892290976ab72933c337781c2655d89a9aff5f
murugan <murugananthamv2005@gmail.com>
2026-09-13 18:59:35 +0530

    precdence of relational and conditional operator

--- fmentals.c
+++ fmentals.c
@@ -183,17 +183,27 @@ int main(void){
 //
 //================================Prcedence of operator
 //  
-    int fun1(){
-        printf("Hi\n");
-        return 1;
+    // int fun1(){
+    //     printf("Hi\n");
+    //     return 1;
+    // }
+    // int fun2(){
+    //     printf("Hello\n");
+    //     return 1;
+    // }
+    // int a;
+    // a=fun1()+fun2();
+    // printf("%d",a);
+//
+//
+//
+    int a=10,b=20,c=30,d=40;
+    if(a <= b == d > c){
+        printf("TRUE");
     }
-    int fun2(){
-        printf("Hello\n");
-        return 1;
+    else{
+        printf("FALSE");
     }
-    int a;
-    a=fun1()+fun2();
-    printf("%d",a);
     return 0;   
 
 
